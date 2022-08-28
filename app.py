97eed2996ff0c265bb0ee9dc83f820cb9ec31bba
Rajat Kaushik <66780684+Rajat-Kaushik99@users.noreply.github.com>
2022-08-28 18:10:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,18 +11,29 @@ def home():
     return render_template('Beekeeping.html')
 
 
-@app.route('/predict',methods=['POST'])
-def predict():
-    test_features=[x for x in request.form.values()]
-    test_features=[eval(i) for i in test_features]
-    final_data=pd.DataFrame([test_features],columns=['Temperature', 'Humidity'])
+@app.route('/predict1',methods=['POST'])
+def predict1():
+    test_features1=[x for x in request.form.values()]
+    test_features1=[eval(i) for i in test_features1]
+    final_data=pd.DataFrame([test_features1],columns=['Temperature', 'Humidity'])
     final_data=pd.DataFrame.to_numpy(final_data)
     
     final_data = final_data.reshape((final_data.shape[0], 1, final_data.shape[1]))
 
     prediction=model.predict(final_data)
-    output=prediction
-    return render_template('Beekeeping.html', prediction_text='Weight of the box is approximately {}'.format(output))
+    output1=prediction
+    
+    test_features2=[x for x in request.form.values()]
+    test_features2=[eval(i) for i in test_features2]
+    final_data=pd.DataFrame([test_features2],columns=['Temperature', 'Humidity'])
+    final_data=pd.DataFrame.to_numpy(final_data)
+    
+    final_data = final_data.reshape((final_data.shape[0], 1, final_data.shape[1]))
+
+    prediction=model.predict(final_data)
+    output2=prediction
+    
+    return render_template('Beekeeping.html', prediction_text='Weight of the box f is approximately {} and Weight of box h is approximately {}'.format(output1,output2))
     
 
 if __name__=="__main__":
